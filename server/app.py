7a297ac87f940000f4b80f96f1d20bfc31b124ee
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-09 13:25:18 -0400

    chatterboxlab done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,81 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/')
+def index():
+    return "Index for Message/ API"
 
-@app.route('/messages/<int:id>')
+@app.route('/messages', methods=['GET','POST'])
+def messages():
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+        response = make_response(
+            messages,
+            200
+        )
+        return response
+    elif request.method == 'POST':
+        new_message = Message(
+            body = request.get_json()["body"],
+            username = request.get_json()["username"],
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        message_dict = new_message.to_dict()
+        response = make_response(
+            message_dict,
+            201
+        )
+        return response
+
+
+@app.route('/messages/<int:id>', methods=['GET','PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if message == None:
+        response_body = {
+            "message": "This record does not exist in our database."
+        }
+        response = make_response(jsonify(response_body),404)
+        return response
+    else:
+        if request.method == 'GET':
+            message_dict = message.to_dict()
+            response = make_response(
+                message_dict,
+                200
+            )
+            return response
+        elif request.method == 'PATCH':
+            for attr in request.form:
+                setattr(message,attr,request.form.get(attr))
+            db.session.add(message)
+            db.session.commit()
+            message_dict = message.to_dict()
+            response = make_response(
+                message_dict,
+                200
+            )
+            return response
+
+        elif request.method == "DELETE":
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "Message deleted."    
+            }
+
+            response = make_response(
+                response_body,
+                200
+            )
+
+            return response
 
 if __name__ == '__main__':
     app.run(port=5555)
